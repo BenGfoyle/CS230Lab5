bed8a655a8736f04fcb73a04a0b0dc488ca50882
BENJAMIN WILLIAM GUILFOYLE <u180621@CS3-04.cs.nuim.ie>
2019-03-14 14:48:31 +0000

    php file, and formatting

---
+++ table.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<?
+function createRow(){
+
+}
+
+function getName(){
+
+}
+
+function update(rowNum){
+
+}
+
+function delete(rowNum){
+
+}
+
+function undo(){
+
+}
+?>
+</body>
+</html>
